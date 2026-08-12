20b69c31a43391f1265485ba240bf537de7b20a3
Shoban Chiddarth <shobanchiddarth.mail@gmail.com>
2026-08-12 12:40:18 +0530

    Updated TODO

--- aws-infra/compute/wg-server.tf
+++ aws-infra/compute/wg-server.tf
@@ -2,3 +2,4 @@
 # wireguard port must be open to the internet
 # initially :22 open to bastion sg but later
 # :22 blocked (WG EC2 can SSH into self (clients) without needing AWS SG approval)
+# Global TODO: for SSH inbound allow only from wireguard server SG
