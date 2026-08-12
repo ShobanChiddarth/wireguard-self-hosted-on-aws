cc7b343662bacbe48248fa8375ee392f03ff71d5
Shoban Chiddarth <shobanchiddarth.mail@gmail.com>
2026-08-12 12:34:44 +0530

    Updated TODO

--- aws-infra/compute/wg-server.tf
+++ aws-infra/compute/wg-server.tf
@@ -1,3 +1,4 @@
 # TODO: create wireguard admin server here
 # wireguard port must be open to the internet
+# initially :22 open to bastion sg but later
 # :22 blocked (WG EC2 can SSH into self (clients) without needing AWS SG approval)
