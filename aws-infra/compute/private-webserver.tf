9b648f672cb373e37d16925dc4bb7ea9d18c76bf
Shoban Chiddarth <shobanchiddarth.mail@gmail.com>
2026-08-12 12:45:38 +0530

    Added private webserver and updated TODO

--- aws-infra/compute/private-webserver.tf
+++ aws-infra/compute/private-webserver.tf
@@ -1,5 +1,79 @@
 # TODO:
-# define web server here
 # :22 must be open to bastion, VPC CIDR
 # :80 must be open to VPC CIDR
 # later :22 must be be open to only VPC CIDR
+
+locals {
+  private_webserver_init = <<-EOF
+    apt-get update
+    apt-get upgrade -y
+    apt-get install -y nginx
+
+    cat > /var/www/html/index.html <<'HTML'
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Private Webserver</title>
+    </head>
+    <body>
+        <h1>Webserver is accessible only to LAN</h1>
+        <h3>If you are seeing this you are in the LAN</h3>
+    </body>
+    </html>
+    HTML
+
+    systemctl enable nginx
+    systemctl restart nginx
+EOF
+}
+
+
+resource "aws_security_group" "private_webserver_sg" {
+    name = "private_webserver_sg"
+    vpc_id = var.vpc_id
+
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [ var.vpc_cidr_block ]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        security_groups = [ aws_security_group.bastion_sg.id ]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [ var.vpc_cidr_block ]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
+
+resource "aws_instance" "private_webserver" {
+    ami = var.ubuntu_ami_id
+    instance_type = "t3.micro"
+    subnet_id = var.private_subnet_id
+
+    user_data = local.private_webserver_init
+    vpc_security_group_ids = [ aws_security_group.private_webserver_sg.id ]
+    key_name = var.management_key_name
+
+    tags = {
+        Name = "private_webserver"
+    }
+}
--- aws-infra/outputs.tf
+++ aws-infra/outputs.tf
@@ -1,3 +1,7 @@
 output "bastion_public_ip" {
     value = module.compute.bastion_public_ip
 }
+
+output "private_webserver_private_ip" {
+    value = module.compute.private_webserver_private_ip
+}
